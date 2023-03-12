45118b1889c82c54c1bb24f7c06c5acbc9138675
Vanillateaa <mohamad.william24@gmail.com>
2023-03-12 19:21:51 +0700

    Update Program.cs

--- mod4_1302213034/Program.cs
+++ mod4_1302213034/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using System;
+
+namespace mod4_1302213034
+{
+    internal class program
+    {
+        public enum datakota
+        {
+            Batununggal,
+            Kujangsari,
+            Mengger,
+            Wates,
+            Cijaura,
+            Jatisari,
+            Margasari,
+            Sekejati,
+            Kebonwaru,
+            Maleer,
+            Ssamoja
+        };
+        public class getkodepos
+        {
+            public static int getkode(datakota kota)
+            {
+                int[]kodepos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273};
+                //list kodepos untuk array
+                return kodepos[(int) kota];
+            }
+        }
+        public static void Main(string[] args)
+        {
+            datakota kota = datakota.Jatisari;
+            int kode = getkodepos.getkode(kota);
+            Console.WriteLine("kelurahan : " + kota + "\nkodepos : " + kode);
+        }
+    }
+}
